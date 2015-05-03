39e483c1a65523649fbd2380ad2bc34f74381dc1
edenzik <edenzik@gmail.com>
2015-05-03 13:56:06 -0400

    async push

--- schematicflow/src/main/java/edu/brandeis/flow/ui/main/FlowUI.java
+++ schematicflow/src/main/java/edu/brandeis/flow/ui/main/FlowUI.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
