4e7e612450559c611096fdc65717dd1defc24c2d
wangyh0728 <wangyh@brandeis.edu>
2015-05-03 23:29:48 -0400

    Join

--- schematicflow/src/main/java/edu/brandeis/flow/core/operator/join/Join.java
+++ schematicflow/src/main/java/edu/brandeis/flow/core/operator/join/Join.java
@@ -3,8 +3,12 @@
  */
 package edu.brandeis.flow.core.operator.join;
 
+import java.util.Iterator;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.google.common.collect.Iterables;
 
 import edu.brandeis.flow.core.operator.JSONOperator;
 
@@ -13,21 +17,13 @@ import edu.brandeis.flow.core.operator.JSONOperator;
  *
  */
 public final class Join extends JSONOperator {
-	String key;
-
+	JSONObject first;
+	JSONObject second;
 	/**
 	 * @param name
 	 */
 	public Join() {
 		super();
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getKey() {
-		return key;
 	}
 
 	/*
@@ -37,20 +33,65 @@ public final class Join extends JSONOperator {
 	 */
 	@Override
 	public void run() {
-		JSONObject top;
-		JSONObject merged = read();
-		while ((top = read()) != null) {
-			// merge 'top' with 'merged'
-			try {
-				merged.put(key, top.get(key));
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		while(true) {
+			if((first = read()) != null && (second = read())!= null) {
+				if (canJoin()) {
+					send(makeJoin());
+//					System.out.println(makeJoin());
+				}
 			}
 		}
-
-		// send
-		send(merged);
 	}
+	
+	public JSONObject makeJoin(){
+		Iterator keys_second = second.keys();
+		while(keys_second.hasNext()) {
+			String key = keys_second.next().toString();
+			if (!first.has(key)) {
+				try {
+					first.put(key, second.get(key));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return first;
+	}
+	
+	public boolean canJoin(){
+		Iterator keys_first = first.keys();
+		Iterator keys_second;
+		int count = 0;
+		int total = 0;
+		while(keys_first.hasNext()){
+			++total;
+			keys_second = second.keys();
+			String tmp1 = keys_first.next().toString();
+			while(keys_second.hasNext()) {
+				String tmp2 = keys_second.next().toString();
+//				System.out.println(tmp1 + "  " + tmp2);
+				if(tmp1.equalsIgnoreCase(tmp2)) {
+					++count;
+				}
+			}
+		}
+		return 2*count <= total;
+
+	}
+//	
+//	public static void main(String[] args) throws JSONException {
+//		JSONObject first = new JSONObject("{1: lol, 2:lol, 3:lol, 4:lol, 5:lol}");
+//		JSONObject second = new JSONObject("{3: lol, 4:lol, 7:lol, 8:lol, 9:lol}");
+//		JSONObject third = new JSONObject("{1: lol, 2:lol, 5:lol, 4:lol, 3:lol}");
+//		JSONObject fourth = new JSONObject("{1: lol, 9:lol, 6:lol, 412:lol, 777:lol}");
+//		Join test = new Join();
+//		test.receive(first);
+//		test.receive(second);
+//		test.receive(third);
+//		test.receive(fourth);
+//		new Thread(test).start();
+//
+//	}
 
 }
